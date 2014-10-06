1660c885c181e49fbd8145c5d714808a5d3b416a
Dzmitry Paulenka <fantasteg@gmail.com>
2014-10-06 08:31:56 +0300

    184...

--- src/main/euler/tasks/Task_184.java
+++ src/main/euler/tasks/Task_184.java
@@ -1,5 +1,6 @@
 package tasks;
 
+import utils.Point;
 import utils.log.Logger;
 
 //Answer :
@@ -10,7 +11,30 @@ public class Task_184 extends AbstractTask {
         Logger.close();
     }
 
-    public void solving() {
+    public int r = 105;
+    public int r2 = r*r;
 
+    Point ps[] = new Point[40000];
+    int pc = 0;
+    public void solving() {
+        for (int x = -r; x < r; ++x) {
+            int x2 = x*x;
+            for (int y = -r; y < r; ++y) {
+                if (x2 + y*y < r2) {
+                    ps[pc++] = new Point(x, y);
+                }
+            }
+        }
+        System.out.println(pc);
+
+        long res = 0;
+        for (int a = 1; a < pc; ++a) {
+            for (int b = 1; b < pc; ++b) {
+                if (a == b) {
+                    ps[a] = ps[b];
+                }
+            }
+        }
+        System.out.println(res);
     }
 }
