6585f2061f481258d6bbdbc53a22b128836eb89a
Dzmitry Paulenka <fantasteg@gmail.com>
2014-10-06 08:30:31 +0300

    184...

---
+++ src/main/euler/tasks/Task_184.java
@@ -0,0 +1,16 @@
+package tasks;
+
+import utils.log.Logger;
+
+//Answer :
+public class Task_184 extends AbstractTask {
+    public static void main(String[] args) {
+        Logger.init("default.log");
+        Tester.test(new Task_184());
+        Logger.close();
+    }
+
+    public void solving() {
+
+    }
+}
