215e53e8234745e4a91fda224f3c4d7fc1215383
Dzmitry Paulenka <fantasteg@gmail.com>
2015-10-14 14:46:46 +0300

    Itransition opener 2015

--- src/main/opener2015/opener15/Task_28.lua
+++ src/main/opener2015/opener15/Task_28.lua
@@ -2,9 +2,13 @@
 --    return(function(...)_=0;repeat;_=_+1;until#(function(...)for _=1,(...)[(...)[1]+1]{(...)[2]}do(function(...)for _=(...)[2],(...)[1][(...)[1][1]+1]{(...)[1][2]}do;if(...)[1][2]==_^(...)[1][1]+(...)[2]^(...)[1][1]then;(function(...);(...)[1][#(...)[1]+1]=(...)[2];end){(...)[1][3],(...)[2]};end;end;end){...,_}end;return(...)[3];end){(...)._-1,_,{},function(...);return(...)[1]^0.35;end}==(...)._;return _;end){_=4}
 --end
 
+-- answer: 6963472309248
+-- see: http://oeis.org/A003826
+
 function some(set, count)
-    for i = 1, count ^ 0.35 do
-        for j = i, count ^ 0.35 do
+    cnt2 = count ^ 0.35 -- !!! pow(count, 0.35) ~ floor[cube root]
+    for i = 1, cnt2 do
+        for j = i, cnt2 do
             if count == j ^ 3 + i ^ 3 then
                 set[#set + 1] = i; -- add to the end
             end;
@@ -21,7 +25,7 @@ function R()
     return count;
 end
 
---print ((1)[1])
+--print (#{})
 
 (print)(#({
     33, 5, {23},
@@ -30,5 +34,5 @@ end
     end
 }));
 
-print (4.3 ^ 0.35)
+print (27 ^ 0.333)
 R()
