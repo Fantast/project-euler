f5cc901e46eb5cab150d9c10221eb5d1b3ae09fa
Dzmitry Paulenka <fantasteg@gmail.com>
2015-10-26 13:57:01 +0300

    Itransition opener 2015

--- src/main/opener2015/opener15/Task_34_3.java
+++ src/main/opener2015/opener15/Task_34_3.java
@@ -5,9 +5,9 @@ import tasks.Tester;
 import utils.log.Logger;
 import utils.triples.IntTriple;
 
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.List;
 
 //Answer :
 public class Task_34_3 extends AbstractTask {
@@ -27,10 +27,10 @@ public class Task_34_3 extends AbstractTask {
     int primes[][] = new int[30][3];
     boolean can[][][] = new boolean[100][100][100];
 
-    Set<IntTriple> olds = new HashSet<>();
-    Set<IntTriple> tmp = new HashSet<>();
-    Set<IntTriple> news = new HashSet<>();
-    Set<IntTriple> news2 = new HashSet<>();
+    List<IntTriple> olds = new ArrayList<>();
+    List<IntTriple> tmp = new ArrayList<>();
+    List<IntTriple> news = new ArrayList<>();
+    List<IntTriple> news2 = new ArrayList<>();
 
     public void solving() {
 //        createPrimes();
@@ -38,7 +38,7 @@ public class Task_34_3 extends AbstractTask {
         for (int[] p : primes) {
             olds.add(new IntTriple(p[0], p[1], p[2]));
         }
-        news = new HashSet<>(olds);
+        news = new ArrayList<>(olds);
 
         while (news.size() != 0) {
             System.out.println("News: " + news.size() + "; Olds: " + olds.size());
@@ -78,7 +78,18 @@ public class Task_34_3 extends AbstractTask {
 
     int sr[] = new int[3];
     private void putnew(int a, int b, int c1, int c2) {
-        for (int c = c1; c1 < 100; c1 += c2) {
+        for (int c = c1; c < 100; c += c2) {
+            sr[0] = a;
+            sr[1] = b;
+            sr[2] = c;
+            Arrays.sort(sr);
+
+            if (sr[2] < 100 && !can[sr[0]][sr[1]][sr[2]]) {
+                can[sr[0]][sr[1]][sr[2]] = true;
+                news2.add(new IntTriple(sr[0], sr[1], sr[2]));
+            }
+        }
+        for (int c = c2; c < 100; c += c1) {
             sr[0] = a;
             sr[1] = b;
             sr[2] = c;
