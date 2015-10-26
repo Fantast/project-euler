7c849fc53329aa77f55112ed7b194ae1e096a02b
Dzmitry Paulenka <fantasteg@gmail.com>
2015-10-26 22:58:30 +0300

    Itransition opener 2015

--- src/main/opener2015/opener15/Task_34.java
+++ src/main/opener2015/opener15/Task_34.java
@@ -3,8 +3,13 @@ package opener15;
 import tasks.AbstractTask;
 import tasks.Tester;
 import utils.log.Logger;
+import utils.triples.IntTriple;
 
-//Answer :
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+//Answer : 17186264134
 public class Task_34 extends AbstractTask {
     public static void main(String[] args) {
         Logger.init("default.log");
@@ -12,40 +17,47 @@ public class Task_34 extends AbstractTask {
         Logger.close();
     }
 
-    int basic[][] = new int[][]{
+    int primes[][] = new int[][]{
             new int[]{2, 3, 4},
             new int[]{2, 4, 4},
             new int[]{2, 4, 5},
             new int[]{6, 6, 6},
             new int[]{2, 6, 10}
     };
-    int primes[][] = new int[30][3];
     boolean can[][][] = new boolean[100][100][100];
 
+    List<IntTriple> tmp = new ArrayList<>();
+    List<IntTriple> news = new ArrayList<>();
+    List<IntTriple> news2 = new ArrayList<>();
+
+    int index1[][][] = new int[100][100][100];
+    int index2[][][] = new int[100][100][100];
+
+    int index_n1[][] = new int[100][100];
+    int index_n2[][] = new int[100][100];
+
     public void solving() {
-        createPrimes();
         for (int[] p : primes) {
-            can[p[0]][p[1]][p[2]] = true;
+            put(p[0], p[1], p[2]);
+        }
+        swap();
+
+        while (news.size() != 0) {
+            System.out.println("News: " + news.size());
+            news2.clear();
+
+            for (IntTriple tn : news) {
+                combine(tn);
+            }
+            swap();
         }
 
         long res = 0;
-        for (int a = 1; a < 100; ++a) {
-            progress(a);
-            for (int b = a; b < 100; ++b) {
-                for (int c = b; c < 100; ++c) {
+        for (int a = 1; a < 100; a++) {
+            for (int b = a; b < 100; b++) {
+                for (int c = b; c < 100; c++) {
                     if (can[a][b][c]) {
                         res += Long.parseLong("" + a + b + c);
-//                        System.out.println(a + " " + b + " " + c + ": " + res);
-
-                        for (int[] p : primes) {
-                            for (int i = a; i < 100; i += p[0]) {
-                                for (int j = a; j < 100; j += p[1]) {
-                                    for (int k = a; k < 100; k += p[2]) {
-                                        can[i][j][k] = true;
-                                    }
-                                }
-                            }
-                        }
                     }
                 }
             }
@@ -53,21 +65,82 @@ public class Task_34 extends AbstractTask {
         System.out.println(res);
     }
 
-    private void createPrimes() {
-        int ind = 0;
-        for (int[] b : basic) {
-            set(ind++, b[0], b[1], b[2]);
-            set(ind++, b[0], b[2], b[1]);
-            set(ind++, b[1], b[0], b[2]);
-            set(ind++, b[1], b[2], b[0]);
-            set(ind++, b[2], b[0], b[1]);
-            set(ind++, b[2], b[1], b[0]);
+    private void swap() {
+        tmp = news;
+        news = news2;
+        news2 = tmp;
+
+        for (int a = 1; a < 100; ++a) {
+            for (int b = 1; b < 100; ++b) {
+                int cnt = index_n2[a][b];
+                for (int ind = 0; ind < cnt; ++ind) {
+                    index1[a][b][index_n1[a][b]++] = index2[a][b][ind];
+                }
+                index_n2[a][b] = 0;
+            }
         }
     }
 
-    private void set(int ind, int a, int b, int c) {
-        primes[ind][0] = a;
-        primes[ind][1] = b;
-        primes[ind][2] = c;
+    private void multiply(IntTriple tn) {
+        for (int a = tn.a; a < 100; a += tn.a) {
+            for (int b = closest(a, tn.b); b < 100; b += tn.b) {
+                for (int c = closest(b, tn.c); c < 100; c += tn.c) {
+                    if (!can[a][b][c]) {
+                        put(a, b, c);
+                    }
+                }
+            }
+        }
+    }
+
+    void combine(IntTriple t) {
+        putnew(t.a, t.b, t.c);
+        putnew(t.a, t.c, t.b);
+        putnew(t.b, t.c, t.a);
+    }
+
+    void putnew(int a, int b, int c1) {
+        int indexLen = index_n1[a][b];
+        for (int i = 0; i < indexLen; ++i) {
+            int c2 = index1[a][b][i];
+            int c = c1 + c2;
+            if (c < 100 && !can[a][b][c]) {
+                put(a, b, c);
+            }
+        }
+    }
+
+    private void put(int a, int b, int c) {
+        indexx(a, b, c);
+        indexx(a, c, b);
+        indexx(b, a, c);
+        indexx(b, c, a);
+        indexx(c, a, b);
+        indexx(c, b, a);
+        news2.add(sortedTriple(a, b, c));
+    }
+
+    private void indexx(int a, int b, int c) {
+        if (!can[a][b][c]) {
+            can[a][b][c] = true;
+            index2[a][b][index_n2[a][b]++] = c;
+        }
+    }
+
+    private int closest(int a, int b) {
+        if (a % b == 0) {
+            return a;
+        }
+        return (a / b + 1) * b;
+    }
+
+    int st[] = new int[3];
+
+    private IntTriple sortedTriple(int a, int b, int c) {
+        st[0] = a;
+        st[1] = b;
+        st[2] = c;
+        Arrays.sort(st);
+        return new IntTriple(st[0], st[1], st[2]);
     }
 }
