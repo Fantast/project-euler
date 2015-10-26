e3bf4b725b185d602207b0dbaf3bb668aaa8528d
Dzmitry Paulenka <fantasteg@gmail.com>
2015-10-26 10:48:21 +0300

    opener 2015

--- src/main/opener2015/opener15/Task_33.java
+++ src/main/opener2015/opener15/Task_33.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-//Answer :
+//Answer : 56315
 public class Task_33 extends AbstractTask {
     public static void main(String[] args) {
         Logger.init("default.log");
@@ -22,90 +22,59 @@ public class Task_33 extends AbstractTask {
     byte[] need = new byte[need_i.length];
     int mask = Integer.parseUnsignedInt("4278190080");
 
-    byte[] res = new byte[3000];
+    String found = "0110100000100100100110100000101100000110100010100000010001000000110100010100000110110100000101101100110100000101101010001000000110100000101111100110100010100000100110100010100000000110100000101100000110100000101100100110100000101110000110100000101110110110100000101100000001000000110100000101111100110100010100000010110100000101101010110100000101111010110100000101111010110100010100011110110100010100011110001000000110100010100000010110100000101110110110100010100011110110100000101110100110100000101111100110100010100000100110100010100011000001000000110100000101100010110100010100000110110100000101110110110100010100011000110100000101100100110100000101100000110100010100000000110100000101111010110100000101100000110100010100011110000010100110100000100110000001000000110100010100011110001000000110100000101101110110100000101111010110100000101100000110100010100011100001011000001000000110100010100001110110100010100000100110100000101111100001000000110100000101110100110100010100000000110100000101110000110100000101110100110100000101111010110100010100000110110100000101100100001011000001000000110100000101100100110100010100010110001000000110100000101111000110100000101111100110100000101101100110100000101101010110100010100000100110100000101101010001000000110100010100000010110100000101111110110100010100000000110100010100010110110100000101100110110100000101111010110100010100000110110100010100000100110100010100011000001000000110100010100000010001000000110100010100000110110100000101111000110100000101100000001011100000010100110100000100110000001000000110100000101110100110100000101111100110100000101100110110100000101101000110100000101100000001000000110100000101100100110100010100010110001000000110100010100000110110100000101111000110100010100000000110100000101101010110100010100000100110100000101101010001000000110100000101111010110100000101100000001000000110100010100011010110100010100000100110100000101111100110100000101110010001000000110100010100000010110100000101110100110100000101100000110100000101111000110100000101101010110100000101110010110100000101110100110100000101101010001011000001000000110100000101110100110100000101111100110100010100010000110100000101111000110100000101100000110100010100000000110100000101111010110100000101100000110100010100011110000010100110100000100100100110100000101100000110100010100010000001000000110100010100000010110100000101110000110100010100000000110100000101101010110100000101111010110100000101101010110100000101100100110100010100010110110100000101110010001000000110100010100000100110100010100000000110100010100000110110100000101111110110100000101110000110100000101110100001000000110100000101111100110100000101110100110100010100000110110100010100000100110100000101100000110100000101101010110100010100000100001000000110100010100000010110100000101100000110100000101100100110100000101100000110100000101111010110100000101111100110100000101111000001000000110100010100000100110100010100011000110100000101111000110100000101100000001011100001011100001011101";
+    byte[] res = new byte[9000];
 
     public void solving() {
         for (int i = 0; i < need.length; ++i) {
             need[i] = (byte) need_i[i];
         }
 
-//        System.out.println(Arrays.toString(need));
+        //1st round
+//        int a = 0;
+//        int b = Integer.parseUnsignedInt("4294967295");
+//        int c = 1;
+//        int p[][] = new int[512][2];
+//
+//        find(0, 0, a, b, c, p, (byte)0, 1);
 
-        int a = 0;
-        int b = Integer.parseUnsignedInt("4294967295");
-        int c = 1;
-        int p[][] = new int[512][2];
-
-        find(0, 0, a, b, c, p, (byte)0);
-//        String ss = "01101000001001001001101000001011000001101000101000000100010000001101000101000001101101000001011011";
-//        byte[] some = new byte[ss.length()];
-//        for (int i = 0; i < ss.length(); ++i) {
-//            some[i] = (byte)(ss.charAt(i) - '0');
-//        }
-//        List<Byte> ress = compress(some);
+        //2nd round
+//        String ss = found;
+//        int volume = 10;
 //
-//        System.out.println(ress.toString());
-
-//        String ss = "";
-//        int volume = 16;
+//        int ind = ss.length();
 //
-//        for (int check = 1; check < 20; ++check) {
-//            if (check == 10) {
-//                volume = 20;
-//            } else if (check == 11) {
-//                volume = 23;
-//            } else if (check == 12) {
-//                volume = 20;
-//            } else {
-//                volume = 16;
-//            }
-//
-//            int ind = ss.length();
-//            String newstring = null;
-//            int fitcount = 0;
-//
+//        for (int v = 10; v < 20; ++v) {
+//            volume = v;
 //            byte[] some = new byte[ss.length() + volume];
 //            for (int i = 0; i < ss.length(); ++i) {
 //                some[i] = (byte)(ss.charAt(i) - '0');
 //            }
-//
 //            for (int i = 0; i < (1<<volume); ++i) {
 //                for (int j = 0; j < volume; ++j) {
 //                    some[ind+j] = (byte)((i >> (volume-1-j)) & 1);
 //                }
 //                List<Byte> res = compress(some);
-//                boolean fit = res.size() >= check;
-//                for (int j = 0; fit && j < check; ++j) {
+//                boolean fit = res.size() == need.length;
+//                for (int j = 180; fit && j < need.length; ++j) {
 //                    if (res.get(j) != need[j]) {
 //                        fit = false;
 //                    }
 //                }
 //                if (fit) {
-//                    fitcount++;
-//                    if (newstring == null) {
-//                        newstring = join(some);
-//                    } else {
-//                        newstring = common(newstring, join(some));
-//                    }
-////                    if (fitcount > 5) {
-////                        break;
-////                    }
-////                    Console.WriteLine(String.Join("", some) + ": " + String.Join(", ", res));
+//                    System.out.println(join(some, some.length));
+//                    break;
 //                }
 //            }
-//            if (newstring == null) {
-//                System.out.println("Ouch");
-//                break;
-//            }
-//
-//            ss = newstring;
-//
-//            String chs = "" + check;
-//            if (check < 100) { chs = "0" + chs;}
-//            if (check < 10) { chs = "0" + chs;}
-//
-//            System.out.println("Check: " + chs + ": " + ss);
 //        }
+
+        //3rd round
+        int sum = 0;
+        for (int i = 0; i < found.length() / 9; ++i) {
+            String ns = found.substring(i*9+1, i*9+9);
+            sum += Integer.parseInt(ns, 2);
+        }
+        System.out.println(sum);
     }
 
     public static String join(byte[] a, int len) {
@@ -163,31 +132,38 @@ public class Task_33 extends AbstractTask {
         return result;
     }
 
-    public boolean find(int needInd, int resInd, int a, int b, int c, int[][] p, byte x) {
-        if (resInd >= res.length || (resInd > (needInd+1) * 30)) {
+    public boolean find(int needInd, int resInd, int a, int b, int c, int[][] p, byte x, int zeroes) {
+        if (resInd >= res.length) {
+            return false;
+        }
+
+        if (zeroes > 10) {
+            return false;
+        }
+
+        int maxSearch = found.length() + (needInd-100 + 1) * 80;
+        if (resInd > found.length() && resInd > maxSearch) {
             return false;
         }
         res[resInd] = x;
-//        if (needInd == 20) {
-//            System.out.println("Almost there 20: " + join(res, resInd));
-//        } else if (needInd == 40) {
-//            System.out.println("Almost there 40: " + join(res, resInd));
-//        } else if (needInd == 60) {
-//            System.out.println("Almost there 60: " + join(res, resInd));
-//        } else
-//            if (needInd == 80) {
-//            System.out.println("Almost there 80: " + join(res, resInd));
-//        } else if (needInd == 100) {
-//            System.out.println("Almost there 100: " + join(res, resInd));
-//        } else if (needInd == 120) {
-//            System.out.println("Almost there 120: " + join(res, resInd));
-//        } else
-        if (needInd == 140) {
+        if (needInd == 20) {
+            System.out.println("Almost there 20: " + join(res, resInd));
+        } else if (needInd == 40) {
+            System.out.println("Almost there 40: " + join(res, resInd));
+        } else if (needInd == 60) {
+            System.out.println("Almost there 60: " + join(res, resInd));
+        } else if (needInd == 80) {
+            System.out.println("Almost there 80: " + join(res, resInd));
+        } else if (needInd == 100) {
+            System.out.println("Almost there 100: " + join(res, resInd));
+        } else if (needInd == 120) {
+            System.out.println("Almost there 120: " + join(res, resInd));
+        } else if (needInd == 140) {
             System.out.println("Almost there 140: " + join(res, resInd));
         } else if (needInd == 160) {
             System.out.println("Almost there 160: " + join(res, resInd));
-        } else if (needInd >= 180) {
-            System.out.println("Almost there 180: " + join(res, resInd));
+        } else if (needInd == 185) {
+            System.out.println("Almost there 185: " + join(res, resInd));
         }
 
         int m = (a + ((b - a) >>> 12) * (4096 * (p[c][1] + 1) / (p[c][0] + p[c][1] + 2)));
@@ -204,6 +180,9 @@ public class Task_33 extends AbstractTask {
         if ((c += c + x) >= 512) c = 1;
         while (((a ^ b) & mask) == 0) {
             byte next = (byte)(b >> 24);
+            if (needInd == need.length - 2) {
+
+            }
             if (next != need[needInd++]) {
                 return false;
             }
@@ -211,14 +190,18 @@ public class Task_33 extends AbstractTask {
             b = (b << 8) + 255;
         }
 
-        byte nextx = (byte) random.nextInt(2);
-
-        boolean ff1 = find(needInd, resInd+1, a, b, c, pcopy(p), nextx);
-        if (ff1) {
-            return true;
+        if (needInd == need.length - 1) {
+            return false;
         }
-        boolean ff0 = find(needInd, resInd+1, a, b, c, pcopy(p), (byte) (1-nextx));
-        return ff0;
+
+        if (resInd < found.length() - 1) {
+            byte foundRes = (byte) (found.charAt(resInd+1) - '0');
+            return find(needInd, resInd+1, a, b, c, pcopy(p), foundRes, foundRes == 0 ? zeroes + 1 : 0);
+        } else {
+            byte nextx = (byte) random.nextInt(2);
+            return find(needInd, resInd+1, a, b, c, pcopy(p), nextx, nextx == 0 ? zeroes + 1 : 0) ||
+                    find(needInd, resInd+1, a, b, c, pcopy(p), (byte) (1-nextx), nextx == 1 ? zeroes + 1 : 0);
+        }
     }
 
     private int[][] pcopy(int[][] p) {
